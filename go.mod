30cbdcb3852009bab305f72251e117218f4c3c8d
Alena <helenavertin@gmail.com>
2026-02-21 17:36:15 +0300

    Initial commit: Simple Go console app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Vertinska/my-first-app
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+    fmt.Println("Моё первое приложение на Go!")
+}
+
